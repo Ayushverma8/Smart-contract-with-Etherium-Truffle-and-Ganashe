fddb6ca451c67d048772b17b21aae8fab1c87d36
Ayush Verma <ay322266@dal.ca>
2022-07-26 06:36:04 -0300

    Added data with signatures

--- index.js
+++ index.js
@@ -4,9 +4,18 @@
 // import { AbortController } from "node-abort-controller";
 
 // const client = create();
+import { createRequire } from "module";
 
+const require = createRequire(import.meta.url);
+import { create } from "ipfs-http-client";
+const client = create();
+import { generateKeyPairSync, sign } from "crypto";
+
+import { AbortController } from "node-abort-controller";
+const crypto = require("crypto");
+
+global.AbortController = AbortController;
 var Web3 = require("web3");
-
 var callStoreOnLocalGanache = async function () {
   //let web3 = new Web3("http://localhost:7545");
   const web3 = new Web3(
@@ -24,56 +33,123 @@ var callStoreOnLocalGanache = async function () {
   });
   console.log(myContract);
   // Setting up public and private keys here for the transaction
+  const keyOptions = [
+    {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: "spki",
+        format: "pem",
+      },
+      privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem",
+      },
+    },
+    {
+      modulusLength: 2048,
+      publicKeyEncoding: {
+        type: "spki",
+        format: "pem",
+      },
+      privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem",
+      },
+    },
+  ];
+
+  const [
+    { publicKey: publicKeyBuyer, privateKey: privateKeyBuyer },
+    { publicKey: publicKeySeller, privateKey: privateKeySeller },
+  ] = keyOptions.map((options) => generateKeyPairSync("rsa", options));
   // Setup done for the public an private key
 
-  myContract.methods
-    .getBuyerSignature_024()
-    .call({ from: web3.eth.defaultAccount })
-    .then(function (recippt) {
-      console.log(
-        "Retrieving the Transaction via the ganache NET",
-        JSON.stringify(recippt, null, 4)
-      );
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // Setting up reading from the IPFS
+  var MFS_path = "/agreement";
+  client.files
+    .write(
+      MFS_path,
+      new TextEncoder().encode(
+        "This agreement is between robert and jackson for purchase of dairy farm ice cream"
+      ),
+      { create: true }
+    )
+    .then(async (r) => {
+      client.files.stat(MFS_path, { hash: true }).then(async (r) => {
+        let ipfsAddr = r.cid.toString();
+        console.log("added file ipfs:", ipfsAddr);
+        // console.log("created message on IPFS:", cid);
+        const resp = await client.cat(ipfsAddr);
+        let content = [];
+        for await (const chunk of resp) {
+          content = [...content, ...chunk];
+          const raw = Buffer.from(content).toString("utf8");
+          const signature_buyer = crypto.sign("sha256", Buffer.from(raw), {
+            key: privateKeySeller,
+            padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+          });
+          myContract.methods
+            .getBuyerSignature_024()
+            .call({ from: web3.eth.defaultAccount })
+            .then(function (recippt) {
+              console.log(
+                "Retrieving the Transaction via the ganache NET",
+                JSON.stringify(recippt, null, 4)
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+            });
 
-  myContract.methods
-    .setBuyerSignature_024("Ayush Verma 2 test")
-    .call({ from: web3.eth.defaultAccount })
-    .then(function (recippt) {
-      console.log(
-        "Submission of the Agreement is being processed via the ganache NET",
-        JSON.stringify(recippt, null, 4)
-      );
+          myContract.methods
+            .setBuyerSignature_024("Ayush Verma 2 test")
+            .call({ from: web3.eth.defaultAccount })
+            .then(function (recippt) {
+              console.log(
+                "Submission of the Agreement is being processed via the ganache NET",
+                JSON.stringify(recippt, null, 4)
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          myContract.methods
+            .getSellerSignature_024()
+            .call({ from: web3.eth.defaultAccount })
+            .then(function (recippt) {
+              console.log(
+                "Retrieving the Transaction via the ganache NET",
+                JSON.stringify(recippt, null, 4)
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          myContract.methods
+            .setSellerSignature_024("Namaste")
+            .call({ from: web3.eth.defaultAccount })
+            .then(function (recippt) {
+              console.log(
+                "Retrieving the Transaction via the ganache NET",
+                JSON.stringify(recippt, null, 4)
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          console.log(signature_buyer.toString("base64"));
+          const signature_seller = crypto.sign("sha256", Buffer.from(raw), {
+            key: privateKeySeller,
+            padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+          });
+          console.log(signature_seller.toString("base64"));
+        }
+
+        // console.log(content.toString());
+      });
     })
-    .catch((error) => {
-      console.log(error);
-    });
-  myContract.methods
-    .getSellerSignature_024()
-    .call({ from: web3.eth.defaultAccount })
-    .then(function (recippt) {
-      console.log(
-        "Retrieving the Transaction via the ganache NET",
-        JSON.stringify(recippt, null, 4)
-      );
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  myContract.methods
-    .setSellerSignature_024("Namaste")
-    .call({ from: web3.eth.defaultAccount })
-    .then(function (recippt) {
-      console.log(
-        "Retrieving the Transaction via the ganache NET",
-        JSON.stringify(recippt, null, 4)
-      );
-    })
-    .catch((error) => {
-      console.log(error);
+    .catch((e) => {
+      console.log(e);
     });
 };
 
